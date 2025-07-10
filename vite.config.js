db97f3a7893931931aeb9a541e5278a630852905
JAYANTH <jayanth@JAYANTHs-MacBook-Air.local>
2025-07-10 13:35:25 +0530

    Fix GitHub Pages build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LIFE-CARE-/', // 👈 Your GitHub repo name
   plugins: [react()],
-})
+});
